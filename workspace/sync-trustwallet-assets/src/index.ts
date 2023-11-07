10ece12fe718767b0104e4460b33d279954e0c4a
Fuxing Loh <git@fuxing.dev>
2023-11-07 15:55:25 +0800

    chore(workflow): update tests

--- workspace/sync-trustwallet-assets/src/index.ts
+++ workspace/sync-trustwallet-assets/src/index.ts
@@ -1,12 +1,23 @@
 import { join } from 'node:path';
 import process from 'node:process';
 
-import { Eip155Sync } from './sync';
+import { Eip155Erc20Sync } from './sync';
 
 export async function sync(): Promise<void> {
   const cwd = process.cwd();
 
-  await new Eip155Sync(join(cwd, 'repo/blockchains/ethereum/assets'), join(cwd, '../../packages/eip155/1')).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/ethereum/assets'),
+    join(cwd, '../../packages/eip155/1/erc20'),
+  ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/polygon/assets'),
+    join(cwd, '../../packages/eip155/137/erc20'),
+  ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/avalanchec/assets'),
+    join(cwd, '../../packages/eip155/43114/erc20'),
+  ).sync();
 }
 
 void sync();
