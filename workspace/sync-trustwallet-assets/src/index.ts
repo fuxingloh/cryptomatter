b36ef6f90b265db9f2f43a3cad0c69fd1f5b632b
Fuxing Loh <git@fuxing.dev>
2023-11-09 16:38:25 +0800

    chore(workspace): map all levain supported eip155/erc20 assets

--- workspace/sync-trustwallet-assets/src/index.ts
+++ workspace/sync-trustwallet-assets/src/index.ts
@@ -18,6 +18,32 @@ export async function sync(): Promise<void> {
     join(cwd, 'repo/blockchains/avalanchec/assets'),
     join(cwd, '../../packages/eip155-43114-erc20/frontmatter'),
   ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/smartchain/assets'),
+    join(cwd, '../../packages/eip155-56-erc20/frontmatter'),
+  ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/arbitrum/assets'),
+    join(cwd, '../../packages/eip155-42161-erc20/frontmatter'),
+  ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/optimism/assets'),
+    join(cwd, '../../packages/eip155-10-erc20/frontmatter'),
+  ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/aurora/assets'),
+    join(cwd, '../../packages/eip155-1313161554-erc20/frontmatter'),
+  ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/celo/assets'),
+    join(cwd, '../../packages/eip155-42220-erc20/frontmatter'),
+  ).sync();
+  await new Eip155Erc20Sync(
+    join(cwd, 'repo/blockchains/base/assets'),
+    join(cwd, '../../packages/eip155-8453-erc20/frontmatter'),
+  ).sync();
+
+  // TODO: tron -> tip474:728126428
 }
 
 void sync();
